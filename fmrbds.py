48bd8b8b23ea7b15f1db7f5532c21e22abb064bc
ben <batgold@gmail.com>
2017-12-06 14:12:34 -0800

    RDS: getting closer

--- fmrbds.py
+++ fmrbds.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
+import pickle
 import commpy as com
 import numpy as nmp
 import matplotlib.pyplot as plt
 import scipy.signal as sig
 from rtlsdr import RtlSdr
 
-sdr = RtlSdr()
 
 F_SAMPLE = int(228e3)       # 225-300 kHz and 900 - 2032 kHz
 F_CENTER = int(96.5e6)      # FM Station, need strong RDBS signal, SF
@@ -21,6 +21,7 @@ print("Samples/sec", str(sps))
 
 
 def rtl_sample():
+    """STEP 1: SAMPLE DATA FROM RTLSDR"""
     sdr.sample_rate = F_SAMPLE
     sdr.center_freq = F_CENTER
     sdr.gain = 'auto'
@@ -29,48 +30,24 @@ def rtl_sample():
     return smp_rtl[2000:]               #first 2000 samples are bogus
 
 
-class Demod:
-    """RF Demodulation to Baseband"""
-
-    def __init__(self,data):
-        self.data = []
-
-    def demod_fm(smp):
-        fm_cmplx = smp[1:] * nmp.conj(smp[:-1])  # 1:end, 0:end-1
-        return nmp.angle(fm_cmplx)
-
-
-    def clock_recovery(smp):
-        crr = sig.lfilter(filters.ipf()[0], filters.ipf()[1], smp)      # filter out 19 kHz
-        crr3 = sig.hilbert(crr) ** 3.0              # multiply up to 57 kHz
-        return crr, crr3
-
-
-    def mix_and_decimate(smp):
-        smp_mix = smp * crr3         # mix signal down by 57 kHz
-        smp_bb = sig.decimate(smp_mix, DEC_RATE, zero_phase=True)  # decimate
-        return smp_bb*1000
-
-
 class Filter:
     """Construct Filters"""
 
     def rrc(self):
         """Cosine Filter"""
-        rrc = [-0.00281562025759368,-0.00243888516957360,-0.000999844542191435,0.00107593094803098,0.00303973034012925,0.00406700703874643,0.00357325036472412,0.00148757358716287,-0.00162769040855969,-0.00468282782128020,-0.00639101106088724,-0.00573885664637510,-0.00244729848210667,0.00275023551791118,0.00815158917037663,0.0115038199095970,0.0107291667736578,0.00477805894125585,-0.00564522027360720,-0.0177416940767021,-0.0268422464557264,-0.0272355771946698,-0.0134654388344483,0.0181901542149565,0.0684322485815653,0.134211232278632,0.208806091825802,0.282774215523415,0.345612930084173,0.387782741962203,0.402633696835896,0.387782741962203,0.345612930084173,0.282774215523415,0.208806091825802,0.134211232278632,0.0684322485815653,0.0181901542149565,-0.0134654388344483,-0.0272355771946698,-0.0268422464557264,-0.0177416940767021,-0.00564522027360720,0.00477805894125585,0.0107291667736578,0.0115038199095970,0.00815158917037663,0.00275023551791118,-0.00244729848210667,-0.00573885664637510,-0.00639101106088724,-0.00468282782128020,-0.00162769040855969,0.00148757358716287,0.00357325036472412,0.00406700703874643,0.00303973034012925,0.00107593094803098,-0.000999844542191435,-0.00243888516957360,-0.00281562025759368]
-        #N = int(30)
-        #T = 1/1187.5/2
-        #alfa = 1 #put 8 samples per sym period. i.e. 16+1 in the main lobe
-        #t,rrc = com.filters.rrcosfilter(N,alfa,T,F_SAMPLE/DEC_RATE)
+        N = int(120)
+        T = 1/1187.5*3/8
+        alfa = 1 #put 8 samples per sym period. i.e. 16+1 in the main lobe
+        __,rrc = com.filters.rrcosfilter(N,alfa,T,F_SAMPLE/DEC_RATE)
         return rrc
 
 
     def bpf(self):
         """Bandpass Filter, at 57kHz"""
-        #slightly wider (3kHz)
-        w = [(F_PILOT*3 - 3e3) / F_SAMPLE*2, (F_PILOT*3 + 3e3) / F_SAMPLE*2]
-        b, a = sig.butter(N=12, btype='bandpass', Wn=w, analog=False)
-        return b,a
+        cutoff = 3.0e3          # one-sided cutoff freq, slightly larger than 2.4kHz
+        w = [(F_PILOT*3 - cutoff) / F_SAMPLE*2, (F_PILOT*3 + cutoff) / F_SAMPLE*2]
+        b, a = sig.butter(N=12, Wn=w, btype='bandpass', analog=False)
+        return b, a
 
 
     def ipf(self):
@@ -81,29 +58,74 @@ class Filter:
         return b, a
 
 
+class Demod:
+    """STEP 2: RF Demodulation to Baseband"""
+
+    def __init__(self, data):
+        self.data = data
+        self.fm_real = []
+        self.crr = []
+        self.crr3 = []
+        self.rds_bb = []
+        self.rds_sym = []
+        self.rds_I = []
+        self.rds_Q = []
+        self.rds_bit = []
+        self.demod_fm()
+
+    def demod_fm(self):
+        """STEP 2.1: DEMODULATE FM"""
+        fm_cmplx = self.data[1:] * nmp.conj(self.data[:-1])                     # 1:end, 0:end-1
+        self.data = nmp.angle(fm_cmplx)
+        self.clock_recovery()
+
+    def clock_recovery(self):
+        """STEP 2.2: RECOVER RF CARRIER"""
+        self.crr = sig.lfilter(filters.ipf()[0], filters.ipf()[1], self.data)   # filter out 19 kHz
+        self.crr3 = sig.hilbert(self.crr) ** 3.0                                # multiply up to 57 kHz
+        #self.crr3 = nmp.exp(nmp.arange(0,len(self.data))*1j*F_PILOT*3)          # free-running clock
+        self.cull_rds()
+
+    def cull_rds(self):
+        """STEP 2.3: FILTER, MIX DOWN TO BB & DECIMATE"""
+        rds_57 = sig.lfilter(filters.bpf()[0], filters.bpf()[1], self.data)     # BPF
+        self.rds_bb = 1000 * rds_57 * self.crr3                                 # mix signal down by 57 kHz
+        self.rds_bb = sig.decimate(self.rds_bb, DEC_RATE, zero_phase=True)      # Decimate
+        self.pulse_shape()
+
+    def pulse_shape(self):
+        """STEP 2.4: APPLY R.R.COSINE FILTER"""
+        self.rds_sym = 5 * sig.lfilter(filters.rrc(), 1, self.rds_bb)
+        self.rds_I = nmp.real(self.rds_sym)
+        self.rds_Q = nmp.imag(self.rds_sym)
+        self.rds_phz = nmp.arctan2(self.rds_I, self.rds_Q)
+
+    def bit_sync(self):
+        return None
+
 filters = Filter()
 
-smp_rtl = rtl_sample()                              # RTL Samples
+live = False
 
-bb = Demod(smp_rtl)
-quit()
+if live:
+    sdr = RtlSdr()
+    smp_rtl = rtl_sample()                              # RTL Samples
 
-smp_fm = demod_fm(smp_rtl)                          # FM Demodulate
-smp_rbds = sig.lfilter(filters.bpf()[0], filters.bpf()[1], smp_fm)      # Bandpass filter RBDS
+    with open('fm_data', 'wb') as f:
+        pickle.dump(smp_rtl, f)
+else:
+    with open('fm_data', 'rb') as f:
+        smp_rtl = pickle.load(f)
 
-crr,crr3 = clock_recovery(smp_fm)                   # Pilot tone extraction
 
-smp_bb = mix_and_decimate(smp_rbds)                 # Mix down to baseband
-smp_bpc = sig.lfilter(filters.rrc(),1,smp_bb)                 # Bi-Phase Code
+demod = Demod(smp_rtl)
 
-#sym_bit = (smp_bpc > 0)
-ang = nmp.arctan2(nmp.real(smp_bpc),nmp.imag(smp_bpc))
 
-phz = 7
-symR = nmp.real(smp_bpc[phz::sps])
-symI = nmp.imag(smp_bpc[phz::sps])
+phz = 4
+symR = nmp.real(demod.rds_sym[phz::sps])
+symI = nmp.imag(demod.rds_sym[phz::sps])
 
-ang_sps = ang[phz::sps]
+ang_sps = demod.rds_phz[phz::sps]
 sym = (ang_sps > 0)
 
 bits = nmp.bitwise_xor(sym[1:], sym[:-1])
@@ -145,21 +167,28 @@ for i in block_sync:
         n += 1
         print i[1]
 
+
 print(n, N_SAMPLES/DEC_RATE/sps/26)
 # PLOTS
 plt.figure()
-plt.scatter(symR,symI)
+plt.plot(symI, symR, 'b.', alpha=0.5)
+#plt.scatter(demod.rds_I, demod.rds_Q)
 
-n1 = sps*1000
-n2 = n1 + sps*10
-f, (p1, p2, p3, p4, p5) = plt.subplots(5)
-p1.plot((smp_rbds[n1*DEC_RATE:n2*DEC_RATE]))
-p2.plot(smp_bb[n1:n2])
-p3.plot(smp_bpc[n1:n2])
-p4.plot(ang[n1:n2])
-p4.plot(nmp.arange(phz,sps*10+phz,sps),ang_sps[n1/sps:n2/sps],'r+')
-p5.plot(nmp.real(crr[n1*DEC_RATE:n2*DEC_RATE]))
+plt.figure()
+plt.plot(((demod.rds_sym[6000:9160])),'go')
+plt.plot(nmp.real((demod.rds_sym[6000:9160])),'b.')
+plt.plot((demod.rds_phz[6000:9160]),'r+')
 
+#n1 = sps*1000
+#n2 = n1 + sps*10
+#f, (p1, p2, p3, p4, p5) = plt.subplots(5)
+#p1.plot((demod.fm_real[n1*DEC_RATE:n2*DEC_RATE]))
+#p2.plot(demod.rds_bb[n1:n2])
+#p3.plot(demod.rds_sym[n1:n2])
+#p4.plot(demod.rds_phz[n1:n2])
+##p4.plot(nmp.arange(phz,sps*10+phz,sps),ang_sps[n1/sps:n2/sps],'r+')
+#p5.plot(nmp.real(demod.crr[n1*DEC_RATE:n2*DEC_RATE]))
+#
 #plt.figure()
 #plt.semilogx(w/2/nmp.pi,20*nmp.log10(abs(h)))
 #plt.psd(smp_bpc[phz::sps], NFFT=2048, Fs=F_SAMPLE/1000/DEC_RATE, Fc=0)
